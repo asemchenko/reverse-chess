409727d97fab9f4eb95a1844a0dbbad6f84bed8e
Asem <andriysemchenko@gmail.com>
2019-03-14 01:01:02 +0200

    added Pawn & tests for it

--- src/main/java/model/chess/chessmans/Pawn.java
+++ src/main/java/model/chess/chessmans/Pawn.java
@@ -1,25 +1,22 @@
 package model.chess.chessmans;
 
-import model.chess.Chessboard;
 import model.chess.ChessmanColor;
 import model.chess.Position;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Objects;
-
 // TODO как реализовать изменение типа фигуры, когда пешка достигает конца поля???
 public class Pawn extends Chessman {
-    public Pawn(@NotNull Chessboard board, @NotNull ChessmanColor color, @NotNull Position position) {
-        board.super(color, position);
+    public Pawn(ChessmanColor color, Position position) {
+        super(color, position);
     }
 
     @Override
-    public boolean canBeMoved(@NotNull Position dst) {
+    protected boolean isReachable(Position dst) {
         return checkInitialBigMove(dst)
-                || checkCapture(dst)
                 || checkSimpleMove(dst);
     }
 
+    // FIXME возможно есть способ уменьшить кол-во кода
     private boolean checkInitialBigMove(@NotNull Position dst) {
         return checkInitialBigMoveBlack(dst) || checkInitialBigMoveWhite(dst);
     }
@@ -36,37 +33,19 @@ public class Pawn extends Chessman {
         return position.getCharPosition() == dst.getCharPosition() && dst.getNumericPosition() == 5;
     }
 
-    private boolean checkCapture(@NotNull Position dst) {
-        return checkCaptureWhite(dst) || checkCaptureBlack(dst);
-    }
-
-    private boolean checkCaptureWhite(@NotNull Position dst) {
-        return color.equals(ChessmanColor.WHITE)
-                && Objects.nonNull(get(dst))
-                && dst.numericDistance(position) == 1
-                && position.charDistance(dst) == 1;
-    }
-
-    private boolean checkCaptureBlack(@NotNull Position dst) {
-        return color.equals(ChessmanColor.BLACK)
-                && Objects.nonNull(get(dst))
-                && dst.numericDistance(position) == -1
-                && position.charDistance(dst) == 1;
-    }
-
     private boolean checkSimpleMove(@NotNull Position dst) {
         return checkSimpleMoveWhite(dst) || checkSimpleMoveBlack(dst);
     }
 
     private boolean checkSimpleMoveWhite(@NotNull Position dst) {
         return color.equals(ChessmanColor.WHITE)
-                && dst.numericDistance(position) == 1
-                && dst.charDistance(position) == 0;
+                && dst.numericSubstract(position) == 1
+                && dst.charSubstract(position) == 0;
     }
 
     private boolean checkSimpleMoveBlack(@NotNull Position dst) {
         return color.equals(ChessmanColor.BLACK)
-                && dst.numericDistance(position) == -1
-                && dst.charDistance(position) == 0;
+                && dst.numericSubstract(position) == -1
+                && dst.charSubstract(position) == 0;
     }
 }
---
+++ src/test/java/model/chess/chessmans/PawnTest.java
@@ -0,0 +1,176 @@
+package model.chess.chessmans;
+
+import model.chess.ChessmanColor;
+import model.chess.Position;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class PawnTest extends ChessmanTest {
+
+    protected void setConstructorWithColor(ChessmanColor color) {
+        setConstructor((s) -> new Pawn(color, s));
+    }
+
+    Stream<Arguments> colorAndStartPositionAndReachableCellsProvider() {
+        // TODO как будет обрабатыватся захват пешкой какой-то фигуры? Пешка ведь бьет не так же как ходит
+        return Stream.of(
+                // WHITE INITIAL MOVE
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('e', 2),
+                        Arrays.asList(
+                                new Position('e', 3),
+                                new Position('e', 4)
+                        )
+                ),
+                // BLACK INITIAL MOVE
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('c', 7),
+                        Arrays.asList(
+                                new Position('c', 6),
+                                new Position('c', 5)
+                        )
+                ),
+                // WHITE USUAL MOVE
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('c', 4),
+                        Arrays.asList(
+                                new Position('c', 5)
+                        )
+                ),
+                // BLACK USUAL MOVE
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('g', 5),
+                        Arrays.asList(
+                                new Position('g', 4)
+                        )
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("colorAndStartPositionAndReachableCellsProvider")
+    void testCanBeMovedTrue(ChessmanColor color, Position startPosition, List<Position> reachableCells) {
+        setConstructorWithColor(color);
+        super.testCanBeMovedTrue(startPosition, reachableCells);
+    }
+
+    @ParameterizedTest
+    @MethodSource("colorAndStartPositionAndReachableCellsProvider")
+    void testCanBeMovedFalse(ChessmanColor color, Position startPosition, List<Position> reachableCells) {
+        setConstructorWithColor(color);
+        super.testCanBeMovedFalse(startPosition, reachableCells);
+    }
+
+
+    Stream<Arguments> colorAndStartPositionAndDstPositionAndExpectedRoute() {
+        return Stream.of(
+                // WHITE INITIAL MOVE
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('h', 2),
+                        new Position('h', 4),
+                        Arrays.asList(
+                                new Position('h', 3)
+                        )
+                ),
+                // BLACK INITIAL MOVE
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('f', 7),
+                        new Position('f', 5),
+                        Arrays.asList(
+                                new Position('f', 6)
+                        )
+                ),
+                // WHITE USUAL MOVE
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('d', 2),
+                        new Position('d', 3),
+                        Collections.emptyList()
+                ),
+                // BLACK USUAL MOVE
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('e', 7),
+                        new Position('e', 6),
+                        Collections.emptyList()
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("colorAndStartPositionAndDstPositionAndExpectedRoute")
+    void testRouteWhenExists(ChessmanColor color, Position startPosition, Position dstPosition, List<Position> expectedRoute) {
+        setConstructorWithColor(color);
+        super.testRouteWhenExists(startPosition, dstPosition, expectedRoute);
+    }
+
+    Stream<Arguments> colorAndStartPositionAndUnreachableDstPosition() {
+        return Stream.of(
+                // WHITE BACK MOVE
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('d', 5),
+                        new Position('d', 4)
+                ),
+                // BLACK BACK MOVE
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('a', 6),
+                        new Position('a', 7)
+                ),
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('c', 5),
+                        new Position('c', 3)
+                ),
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('g', 3),
+                        new Position('g', 5)
+                ),
+                arguments(
+                        ChessmanColor.BLACK,
+                        new Position('e', 6),
+                        new Position('d', 5)
+                ),
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('h', 6),
+                        new Position('g', 7)
+                ),
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('e', 2),
+                        new Position('f', 4)
+                ),
+                arguments(
+                        ChessmanColor.WHITE,
+                        new Position('e', 2),
+                        new Position('f', 3)
+                )
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("colorAndStartPositionAndUnreachableDstPosition")
+    void testRouteWhenDoesNotExist(ChessmanColor color, Position startPosition, Position unreachablePosition) {
+        setConstructorWithColor(color);
+        super.testRouteWhenDoesNotExist(startPosition, unreachablePosition);
+    }
+}
